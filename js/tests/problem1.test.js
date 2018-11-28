203c8c0a355f804aeeaa5b31dd4365d672e42767
Brandon Banks <b93banks@gmail.com>
2018-11-27 23:53:00 -0500

    and other js solutions

--- js/tests/problem1.test.js
+++ js/tests/problem1.test.js
@@ -1,17 +1,25 @@
-import {twoNumSum} from '../problems/problem1';
+import {withBruteForce, withAnObject, withASet} from '../problems/problem1';
 
 test('test it should return true if 2 numbers add up to k', () => {
-  expect(twoNumSum([10, 3, 7], 17)).toBe(true);
+  expect(withBruteForce([10, 3, 7], 17)).toBe(true);
+  expect(withAnObject([10, 3, 7], 17)).toBe(true);
+  expect(withASet([10, 3, 7], 17)).toBe(true);
 });
 
 test('test it should return false if 2 numbers dont add up to k', () => {
-  expect(twoNumSum([10, 3, 7], 14)).toBe(false);
+  expect(withBruteForce([10, 3, 7], 14)).toBe(false);
+  expect(withAnObject([10, 3, 7], 14)).toBe(false);
+  expect(withASet([10, 3, 7], 14)).toBe(false);
 });
 
 test('test it should return false even if k is in array', () => {
-  expect(twoNumSum([10, 3, 7], 7)).toBe(false);
+  expect(withBruteForce([10, 3, 7], 7)).toBe(false);
+  expect(withAnObject([10, 3, 7], 7)).toBe(false);
+  expect(withASet([10, 3, 7], 7)).toBe(false);
 });
 
 test('test it can handle duplicate numbers', () => {
-  expect(twoNumSum([7, 7], 14)).toBe(true);
+  expect(withBruteForce([7, 7], 14)).toBe(true);
+  expect(withAnObject([7, 7], 14)).toBe(true);
+  expect(withASet([7, 7], 14)).toBe(true);
 });
