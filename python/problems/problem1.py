dd7454d34eaaa9aa6c1322e461198d295f027438
Brandon Banks <b93banks@gmail.com>
2018-11-27 23:24:04 -0500

    camel case and comments

--- python/problems/problem1.py
+++ python/problems/problem1.py
@@ -1,9 +1,19 @@
+# Problem 1
+#
+# Google
+#
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+#
+# Bonus: Can you do this in one pass?
+
+
 def two_num_sum(numbers, k):
-    solveNums = {}
+    solve_nums = {}
     if len(numbers) < 2:
         return False
     for num in numbers:
-        if num in solveNums:
+        if num in solve_nums:
             return True
-        solveNums[k - num] = True
+        solve_nums[k - num] = True
     return False
